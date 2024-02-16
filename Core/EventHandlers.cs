9d9ef3e9a07bb23348eb912a9649d6c656c77a43
kalebbroo <kalebbroo@gmail.com>
2024-02-15 23:44:21 -0500

    rearranged files and cleaned up program.cs

---
+++ Core/EventHandlers.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.VisualBasic;
+
+namespace HartsyBot.Core
+{
+    public class EventHandlers
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interactions;
+
+        public EventHandlers(DiscordSocketClient client, InteractionService interactions)
+        {
+            _client = client;
+            _interactions = interactions;
+        }
+
+        public void RegisterHandlers()
+        {
+            _client.UserJoined += OnUserJoinedAsync;
+            _client.InteractionCreated += async interaction =>
+            {
+                var ctx = new SocketInteractionContext(_client, interaction);
+                await _interactions.ExecuteCommandAsync(ctx, null);
+            };
+        }
+
+        private async Task OnUserJoinedAsync(SocketGuildUser user)
+        {
+            // Specify the name of the welcome channel
+            string welcomeChannelName = "welcome";
+
+            // Find the welcome channel by its name
+            var welcomeChannel = user.Guild.TextChannels.FirstOrDefault(x => x.Name == welcomeChannelName);
+
+            // Check if the welcome channel is found
+            if (welcomeChannel != null)
+            {
+                // Path to the image
+                var imagePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "images", "welcome.png");
+
+                // Open the image file
+                using var stream = new FileStream(imagePath, FileMode.Open);
+                // Message to be sent, welcoming the user who joined
+                string welcomeMessage = $"{user.Mention}, welcome to the **Hartsy.AI** Discord Server! Check out the #rules channel.";
+
+                // Send the image along with the welcome message
+                await welcomeChannel.SendFileAsync(stream, "welcome.png", welcomeMessage);
+            }
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Discord.Interactions;
 using dotenv.net;
 using dotenv.net.Utilities;
 using System.IO;
+using System.Reflection;
 
 namespace HartsyBot
 {
@@ -39,20 +40,19 @@ namespace HartsyBot
 
             var config = new DiscordSocketConfig
             {
-                GatewayIntents = GatewayIntents.All
+                GatewayIntents = GatewayIntents.All,
+                LogLevel = LogSeverity.Debug
             };
 
             _client = new DiscordSocketClient(config);
             _interactions = new InteractionService(_client); // Initialize InteractionService
 
-            // Register command modules
-            await _interactions.AddModulesAsync(typeof(Program).Assembly, null);
-
-
-            _client.Log += LogAsync;
+            _client.Log += Log;
+            _interactions.Log += Log;
+            _client.Ready += ReadyAsync;
 
             // Initialize and register event handlers
-            var eventHandlers = new EventHandlers(_client, _interactions);
+            var eventHandlers = new Core.EventHandlers(_client, _interactions);
             eventHandlers.RegisterHandlers();
 
             //var token = Environment.GetEnvironmentVariable("BOT_TOKEN"); // Get the bot token from environment variables
@@ -65,33 +65,56 @@ namespace HartsyBot
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            _client.Ready += async () =>
-            {
-                var guild = _client.Guilds.FirstOrDefault(); // Get the first guild the bot is in
-                if (guild != null)
-                {
-                    ulong guildId = guild.Id;
-                    if (guild != null)
-                    {
-                        //await _interactions.AddCommandsToGuildAsync(guildId, true);
-                        await _interactions.RegisterCommandsGloballyAsync(true);
-                    }
-                    else
-                    {
-                        await _interactions.RegisterCommandsGloballyAsync(true);
-                    }
-                }
-            };
-            // Wait for a short period to ensure commands are registered
-            await Task.Delay(1000); // Delay for 1 second
-
-        // Block this task until the program is closed.
-        await Task.Delay(-1);
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
 
-        private Task LogAsync(LogMessage log)
+        private async Task ReadyAsync()
         {
-            Console.WriteLine(log.ToString());
+            try
+            {
+                // Things to be run when the bot is ready
+                if (_client.Guilds.Any())
+                {
+                    // Register command modules with the InteractionService.
+                    // Tells  to scan the whole assembly for classes that define slash commands.
+                    await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+                    // DEBUG: Delete all global commands
+                    //await _client.Rest.DeleteAllGlobalCommandsAsync();
+
+                    // Get the ID of the first guild the bot is a member of
+                    // Then register the commands to that guild
+                    var guildId = _client.Guilds.First().Id;
+                    await _interactions.RegisterCommandsToGuildAsync(guildId, true);
+                    //await _interactions.RegisterCommandsGloballyAsync(true);
+                }
+                else
+                {
+                    Console.WriteLine($"\nNo guilds found\n");
+                }
+
+                Console.WriteLine($"\nLogged in as {_client.CurrentUser.Username}\n" +
+                    $"Registered {_interactions.SlashCommands.Count} slash commands\n" +
+                    $"Bot is a member of {_client.Guilds.Count} guilds\n");
+                await _client.SetGameAsync("/help", null, ActivityType.Listening);
+            }
+            catch (Exception e)
+            {
+                // Log the exception
+                Console.WriteLine($"Exception: {e}");
+                throw;
+            }
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine($"{DateTime.Now} [{message.Severity}] {message.Source}: {message.Message}");
+            if (message.Exception is not null) // Check if there is an exception
+            {
+                // Log the full exception, including the stack trace
+                Console.WriteLine($"Exception: {message.Exception}");
+            }
             return Task.CompletedTask;
         }
     }
