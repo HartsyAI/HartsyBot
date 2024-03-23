3004184360aa621a8dcd7ad387f4b536edd4ca23
kalebbroo <kalebbroo@gmail.com>
2024-03-22 23:56:30 -0400

    rewrote the message during generation. turned into embed.

--- Core/Commands.cs
+++ Core/Commands.cs
@@ -307,7 +307,17 @@ namespace HartsyBot.Core
             if (subStatus != null && userInfo.Credit > 0)
             {
                 bool creditUpdated = await _supabaseClient.UpdateUserCredit(user.Id.ToString(), credits);
-                await FollowupAsync($"You have {credits} GPUT. You will have {credits - 1} GPUT after this image is generated.", ephemeral: true);
+                var embed = new EmbedBuilder()
+                    .WithTitle("Image Generation")
+                    .WithDescription($"You have {credits} GPUT. You will have {credits - 1} GPUT after this image is generated.")
+                    .AddField("Generate Command", "This command allows you to generate images based on the text and template you provide. " +
+                    "Each generation will use one GPUT from your account.")
+                    .WithColor(Discord.Color.Gold)
+                    .WithCurrentTimestamp()
+                    .Build();
+
+                await FollowupAsync(embed: embed, ephemeral: true);
+
                 // Add the role to the user if they do not have it
                 await AddSubRole(user, subStatus);
 
